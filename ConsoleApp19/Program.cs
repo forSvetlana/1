f2bdc17b3761647aa34835c313cfbc23d902d118
Andrey Vologdin <minaev142@gmail.com>
2019-09-15 13:57:34 +0300

    init

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //циклы
+
+            ////с предусловием
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.WriteLine();
+
+            //i = 11;
+            ////с предусловием
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i < 10);
+
+            ////for
+            //for (i = 0; i < 10; i++)
+            //    Console.WriteLine(i);
+            //Console.WriteLine("Введите число ");
+            //int n;
+            //double sum = 0.0;
+            //Int32.TryParse(Console.ReadLine(), out n);
+            //if (n > 0)
+            //{
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        if (i % 2 != 0)
+            //        {
+            //            Console.Write("1/" + i);
+            //            sum += 1 / (double)i;
+            //        }
+            //        else
+            //        {
+            //            Console.Write(" - 1/" + i);
+            //            sum -= 1 / (double)i;
+            //            if (i < n)
+            //                Console.Write(" + ");
+
+            //        }
+
+            //    }
+
+            //    Console.Write(" = " + sum);
+            //}
+            //else
+            //    Console.WriteLine("Нужно положительное число  и целое !!! ");
+
+            //switch
+
+            //int n;
+            //Int32.TryParse(Console.ReadLine(), out n);
+
+            //switch (n)
+            //{
+            //    case 1:
+            //        Console.WriteLine($"Вы ввели {n}");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine($"Вы ввели {n}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Вы ввели неизвестное число");
+            //        break;
+            //}
+
+            // Console.Close() - закрыть окно приложения
+        }
+    }
+}
