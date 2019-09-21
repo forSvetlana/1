6aca63d33b0a9a830a4b601a35b0c9bb82a05553
Andrey Vologdin <minaev142@gmail.com>
2019-09-21 19:19:26 +0300

    ReView HW

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -78,6 +78,131 @@ namespace ConsoleApp19
             //}
 
             // Console.Close() - закрыть окно приложения
+            
+            int move;
+            double num1, num2, num3, step, x1, x2, D;
+
+            Console.WriteLine("1.Сложить.");
+            Console.WriteLine("2.Вычесть.");
+            Console.WriteLine("3.Умножить.");
+            Console.WriteLine("4.Разделить.");
+            Console.WriteLine("5.Квадратный корень числа.");
+            Console.WriteLine("6.Возведение числа в степень.");
+            Console.WriteLine("7.Перевод числа в двоичную СС.");
+            Console.WriteLine("8.Нахождение корней квадратного уравнения.");
+            Console.WriteLine("0.Выход. ");
+
+            int.TryParse(Console.ReadLine(), out move);
+            while (move != 0)
+            {
+                Console.Write("Введите число: ");
+                Double.TryParse(Console.ReadLine(), out num1);
+
+                if ((move >= 1) && (move <= 4))
+                {
+                    Console.Write("Второе число: ");
+                    Double.TryParse(Console.ReadLine(), out num2);
+
+                    if (move == 1)
+                    {
+                        num1 += num2;
+                    }
+
+                    if (move == 2)
+                    {
+                        num1 -= num2;
+                    }
+
+                    if (move == 3)
+                    {
+                        num1 *= num2;
+                    }
+
+                    if (move == 4)
+                    {
+                        if (num2 == 0)
+                        {
+                            num1 = 0;
+                            Console.WriteLine("num2 != 0");
+                        }
+                        else num1 /= num2;
+                    }
+
+                    Console.WriteLine(num1);
+                }
+                if ((move >= 5) && (move <= 7))
+                {
+                    if (move == 5)
+                    {
+                        num1 = Math.Sqrt(num1);
+                        Console.WriteLine(num1);
+                    }
+
+                    if (move == 6)
+                    {
+                        Console.WriteLine("Какая степень числа?");
+                        step = Convert.ToDouble(Console.ReadLine());
+                        num1 = Math.Pow(num1, step);
+                        Console.WriteLine(num1);
+                    }
+
+                    if (move == 7)
+                    {
+                        int mod;
+                        int i = 0;
+                        string result = "";
+
+                        int inum = Convert.ToInt32(num1);
+                        while (inum >= 1)
+                        {
+                            mod = inum % 2;
+                            result += mod.ToString();
+                            i++;
+                            inum /= 2;
+                        }
+
+                        for (int count = result.Length; count > 0; count--)
+                            Console.Write(result[count - 1]);
+
+                        Console.WriteLine();
+                    }
+                }
+                if (move == 8)
+                {
+                    Console.Write("Второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Третье число: ");
+                    num3 = Convert.ToDouble(Console.ReadLine());
+
+                    D = num2 * num2 - 4 * num1 * num3;
+                    D = Math.Sqrt(D);
+                    x1 = (-num2 + D) / (2 * num1);
+                    x2 = (-num2 - D) / (2 * num1);
+
+                    Console.WriteLine("x1 = " + x1);
+                    Console.WriteLine("x2 = " + x2);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("1.Сложить.");
+                Console.WriteLine("2.Вычесть.");
+                Console.WriteLine("3.Умножить.");
+                Console.WriteLine("4.Разделить.");
+                Console.WriteLine("5.Квадратный корень числа.");
+                Console.WriteLine("6.Возведение числа в степень.");
+                Console.WriteLine("7.Перевод числа в двоичную СС.");
+                Console.WriteLine("8.Нахождение корней квадратного уравнения.");
+                Console.WriteLine("0.Выход. ");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Выберите следующее действие. ");
+                move = Convert.ToInt16(Console.ReadLine());
+
+            }
+            Console.Write("Окей ");
         }
     }
 }
